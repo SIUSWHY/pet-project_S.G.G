90e01feca8b7f03b883a1f587d553bffc4b1b549
Daniil <ventsovdaniil@yandex.ru>
2022-03-26 14:19:31 +0300

    add header

---
+++ src/components/Home/MainPage/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="header-align">
+    <div class="header">
+      <div class="header-title">SUPERGIANT GAMES</div>
+    </div>
+    <div class="header-pic"></div>
+  </div>
+</template>
+
+<script lang="ts" src="./index.ts" />
+<style lang="scss" src="./index.scss" scoped />
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <MainPage />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+  import { Component, Vue } from 'vue-property-decorator';
+  import MainPage from '/src/components/Home/MainPage/index.vue'; // @ is an alias to /src
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+  @Component({
+    components: {
+      MainPage,
+    },
+  })
+  export default class Home extends Vue { }
 </script>
