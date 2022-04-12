0061f9f0ed8b6316ddb75f4de8f3f12aacf886e7
daniil <ventsovdaniil@yandex.ru>
2022-04-12 16:36:33 +0300

    fix

--- src/components/Home/MainPage/GameCollection/index.vue
+++ src/components/Home/MainPage/GameCollection/index.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="conteiner">
-    <div class="collection-align">
-      <img src="../../../../assets/bastion.png" alt="pic">
-      <div class="ditails-align">
-        <div class="title">Bastion</div>
-        <p>
-          <b>Bastion</b>
-          is the first title from Supergiant Games, an original action role-playing game set in a lush imaginative
-          world, in which
-          players must create and fight for civilization’s last refuge as a mysterious narrator marks their every move.
-        </p>
+    <div>
+      <img class="bg-pic" src="../../../../assets/bastion-bg.png" alt="pic" />
+      <div class="collection-align">
+        <img src="../../../../assets/bastion.png" alt="pic" />
+        <div class="ditails-align">
+          <div class="title">Bastion</div>
+          <p>
+            <b>Bastion</b>
+            is the first title from Supergiant Games, an original action
+            role-playing game set in a lush imaginative world, in which players
+            must create and fight for civilization`s last refuge as a mysterious
+            narrator marks their every move.
+          </p>
+        </div>
       </div>
     </div>
   </div>
